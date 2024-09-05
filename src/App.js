b9df45dea7d4ebbe88dd0344f599190e0bd0f373
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-05 08:57:55 +0530

    Creatig NavBar , home and about components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import { NavBar } from './components/NavBar';
+import { Home } from './components/Home';
+import { About } from './components/About';
 function App() {
   return (
-    <div className="App">
-      <h1>This is NoteStack</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
