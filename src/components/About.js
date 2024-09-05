7a9802c1154a53bc87cfb21ca58cf0c139c7f5ab
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-05 19:40:15 +0530

    form from bootstrap

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,6 @@
 import React, { useContext, useEffect } from 'react';
-import noteContext from '../context/notes/noteContext';
 export const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-  }, [])
-  
   return (
-    <div>This is About {a.state.name} and is currently pursuing {a.state.program}</div>
+    <div>This is About page</div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,21 +1,7 @@
 import noteContext from "./noteContext";
-import { useState } from "react";
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Zaid",
-        "program": "BCA"
-    }
-    const [state, setstate] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setstate({
-                "name": "Mohammad Zaid Khan",
-                "program": "Bechlore of Computer Application"
-            })
-        }, 3000);
-    }
     return (
-        <noteContext.Provider value={{state,update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
