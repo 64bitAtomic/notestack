3ca4daa106eaf2ae95fc4b6b7ddf75fbcf98932e
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-05 15:23:44 +0530

    Context API in React

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ import {
 import { NavBar } from './components/NavBar';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import NoteState from './context/notes/NoteState';
 function App() {
   return (
     <>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
+
     </>
   );
 }
